10be5bb23966e3d719cda858792948624dee7580
Hancheng Xiao <xiao.hanc@husky.neu.edu>
2017-08-15 19:00:42 -0400

    Add files via upload

---
+++ Selenium with python to test AMP html.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import urllib
+import time
+
+url = "C:\Cross Broswer Drivers\chromedriver_win32\chromedriver.exe"
+driver = webdriver.Chrome(url)
+'''using chrome webdriver of selenium to open the specified url'''
+
+tags=urllib.urlopen('http://www.cnn.com/2017/08/15/politics/dreamhost-department-of-justice-trump-opponents/index.html').read().split('>')
+
+'''get html tags from the specific url and save it as a list'''
+
+driver.get("http://www.cnn.com/2017/08/15/politics/dreamhost-department-of-justice-trump-opponents/index.html")
+
+if tags[0]=="<!DOCTYPE html":
+    print 1
+if driver.find_elements_by_tag_name("html") or driver.find_elements_by_tag_name("html amp"):
+    print 2
+if driver.find_elements_by_tag_name('body') and driver.find_elements_by_tag_name("head"):
+    print 3
+if driver.find_element_by_xpath("//link[@rel='canonical'][@href]"):
+    print 4
+if driver.find_element_by_xpath("//meta[@charset='utf-8']"):
+    print 5
+if driver.find_element_by_xpath("//meta[@name='viewport'][@content='width=device-width, initial-scale=1.0, minimum-scale=1.0']") or driver.find_element_by_xpath("//meta[@name='viewport'][@content='width=device-width, minimum-scale=1']"):
+    print 6
+if driver.find_element_by_xpath("//script[@src='https://cdn.ampproject.org/v0.js']"):
+    print 7
+if driver.find_element_by_xpath("//style[@amp-boilerplate]"):
+    print 8
+
+'''using Xpath to locate each tags and attributes responding to its value, detect if each required format tags exists or not'''
